6bcd258a0f76de307ad73139df9240a45a1854dd
Gustavo <gustavogomes1108@gmail.com>
2026-04-14 10:27:16 -0300

    add-pagina-inicial

--- front-end/kermit-store/src/App.jsx
+++ front-end/kermit-store/src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import KermitLogo from "/logo.png";
+import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="main container-fluid min-vh-100">
+      <div className="d-flex justify-content-center p-2 mt-2 mb-2">
+        <div className="d-flex align-items-center gap-2">
+          <img className="img-fluid" src={KermitLogo} alt="main-logo" />
+          <h1 className="m-0">Kermit Store</h1>
+        </div>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- front-end/kermit-store/src/pages/Home.jsx
+++ front-end/kermit-store/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import KermitLogo from "/logo.png";
 import { MdSort } from "react-icons/md";
 import { CiSearch } from "react-icons/ci";
 
@@ -8,24 +7,17 @@ import "../css/Home.css";
 const Home = () => {
   return (
     <div className="container min-vh-100">
-      <header className="d-flex justify-content-center p-2 mb-2">
-        <div className="d-flex align-items-center gap-1">
-          <img src={KermitLogo} alt="main-logo" />
-          <h1>Kermit Store</h1>
-        </div>
-      </header>
-
       <section>
-        <div className="row gy-2">
-          <div className="col-12 col-sm-6 d-flex gap-2">
-            <button type="button" className="btn btn-primary">
+        <div className="row gy-2 mb-4">
+          <div className="col-12 col-sm-7 d-flex justify-content-center justify-content-sm-start gap-2">
+            <button type="button" className="btn btn-primary" title="Create a new product">
               New product
             </button>
-            <button type="button" className="btn btn-secondary">
+            <button type="button" className="btn btn-secondary" title="Sort products">
               <MdSort />
             </button>
           </div>
-          <div className="col-12 col-sm-6 d-flex justify-content-end">
+          <div className="col-12 col-sm-5 d-flex justify-content-end">
             <div className="search-bar input-group">
               <input
                 type="text"
@@ -35,6 +27,7 @@ const Home = () => {
               <span
                 role="button"
                 className="input-group-text"
+                title="Search the product"
                 onClick={() => alert("Olá")}
               >
                 <CiSearch />
@@ -43,9 +36,131 @@ const Home = () => {
           </div>
         </div>
 
-        <div></div>
+        <div className="row mb-4">
+          <div className="col-12">
+            <div className="table-responsive">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Brand</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Created at</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Image</td>
+                    <td>2025-11-20</td>
+                    <td>R$ 25,00</td>
+                    <td className="d-flex gap-2">
+                      <button
+                        className="btn btn-primary"
+                        title="Edit the product"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-danger"
+                        title="Delete the product"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Image</td>
+                    <td>2025-11-20</td>
+                    <td>R$ 25,00</td>
+                    <td className="d-flex gap-2">
+                      <button
+                        className="btn btn-primary"
+                        title="Edit the product"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-danger"
+                        title="Delete the product"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Image</td>
+                    <td>2025-11-20</td>
+                    <td>R$ 25,00</td>
+                    <td className="d-flex gap-2">
+                      <button
+                        className="btn btn-primary"
+                        title="Edit the product"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-danger"
+                        title="Delete the product"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
 
-        <div></div>
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center">
+            <nav>
+              <ul className="pagination">
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    1
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    2
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    3
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </section>
     </div>
   );
